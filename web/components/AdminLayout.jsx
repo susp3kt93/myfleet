fa516c151103cc937725d1695699b791f5c8840b
Natan <susp3kt@Natans-MacBook-Pro.local>
2026-01-26 19:54:16 +0000

    feat: add company logo to header with click to settings

--- web/components/AdminLayout.jsx
+++ web/components/AdminLayout.jsx
@@ -2,9 +2,12 @@
 
 import { useRouter } from 'next/navigation';
 import { useDispatch, useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
 import { logout } from '../lib/authSlice';
 import { useTranslation } from '../contexts/LanguageContext';
 import LanguageSwitcher from './LanguageSwitcher';
+import Link from 'next/link';
+import api from '../lib/api';
 
 /**
  * AdminLayout - Reusable layout component for all admin pages
